034c8fdcaea604ffe0d1b97fe79dc975c4788ccb
haris96 <haris@fit-outfit.com>
2022-05-13 16:16:40 +0500

    working with internationalization

--- pages/_app.js
+++ pages/_app.js
@@ -24,9 +24,8 @@ function MyApp({ Component, pageProps }) {
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
           integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
-          // eslint-disable-next-line react/no-unknown-property
-          crossorigin='anonymous'
-          referrerpolicy='no-referrer'
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
         />
       </Head>
       <Component {...pageProps} />
