17a56c04c5bb5637618e2744f0098c8e4eb6a173
haris96 <haris@fit-outfit.com>
2022-05-14 13:55:48 +0500

    jwt added to the boiler plate

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,31 @@ import Router from 'next/router'
 import NProgress from 'nprogress'
 import { appWithTranslation } from 'next-i18next'
 import nextI18NextConfig from '../next-i18next.config.js'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary'
 import Spinner from '../components/common/spinner'
 import { wrapper } from '../redux/store'
 import { useDispatch } from 'react-redux'
+import useJwt from '../jwt/jwtService'
+import jwt_decode from 'jwt-decode'
+import { SET_IS_ACCOUNT_TRUE, SET_LOGGED_IN_USER, USER_LOGGED_IN_SUCCESS } from '../redux/action/actionTypes/customer'
 
 NProgress.configure({ showSpinner: false })
 
 function MyApp({ Component, pageProps }) {
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (useJwt.getToken()) {
+      const token = useJwt.getToken()
+      const decode = jwt_decode(token)
+      dispatch({ type: SET_LOGGED_IN_USER, payload: decode })
+      dispatch({ type: USER_LOGGED_IN_SUCCESS, payload: decode })
+    }
+
+    if (localStorage.getItem('isRegistered')) dispatch({ type: SET_IS_ACCOUNT_TRUE })
+  }, [])
 
   const locale = Router.router?.locale
   console.log('locale in _app : ', locale)
